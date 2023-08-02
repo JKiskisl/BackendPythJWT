114499b084d0e570066a83b9fc798a9c571d3a25
JKiskisl <smicscl@gmail.com>
2023-08-02 14:58:41 +0300

    fix expire token

--- app/auth/auth_bearer.py
+++ app/auth/auth_bearer.py
@@ -3,21 +3,28 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from .auth_handler import decodeJWT, refreshJWT
 
-
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
         credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
-        if credentials:
-            if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
-            if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
-            return credentials.credentials
-        else:
+        
+        if not credentials:
             raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+        # Check if it's a logout request
+        if request.url.path.endswith("/user/logout"):
+            return credentials.credentials
+        
+        # For other requests, proceed with token verification
+        if not credentials.scheme == "Bearer":
+            raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+        
+        if not self.verify_jwt(credentials.credentials):
+            raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+        
+        return credentials.credentials
 
     def verify_jwt(self, jwtoken: str) -> bool:
         is_token_valid: bool = False
@@ -31,7 +38,7 @@ class JWTBearer(HTTPBearer):
                 if refreshed_token:
                     is_token_valid = True
                     self.credentials.credentials = refreshed_token["access_token"]
-                    print("token refreshed successfully")
+                    print("Token refreshed successfully")
                     print(refreshed_token)
         except:
             pass
