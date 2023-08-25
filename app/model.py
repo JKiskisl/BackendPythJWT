c4da4faa3ec7ab3b66f92209d5f07e994ae8e443
JKiskisl <smicscl@gmail.com>
2023-08-25 14:17:08 +0300

    upd updatepostschema

--- app/model.py
+++ app/model.py
@@ -24,7 +24,7 @@ class PostSchema(BaseModel):
             "example": {
                 "title": "Securing FastAPI applications with JWT.",
                 "content": "Test!",
-                "date": "2023-08-24",
+                "date": "08/24/2023",
                 "happythings": "Enjoyed a walk in the park.",
                 "waterintake": 5,
                 "todaysmood": 4,
@@ -66,11 +66,34 @@ class UserLoginSchema(BaseModel):
 class UpdatePostSchema(BaseModel):
     title: str = Field(default=None)
     content: str = Field(default=None)
+    date: str = Field(default=None)
+    happythings: str = Field(default=None)
+    waterintake: int = Field(default=None)
+    todaysmood: int = Field(default=None)
+    selfcareActivities: str = Field(default=None)
+    Breakfast: str = Field(default=None)
+    Lunch: str = Field(default=None)
+    Dinner: str = Field(default=None)
+    Snacks: str = Field(default=None)
+    Anxious: str = Field(default=None)
+    Sad: str = Field(default=None)
+    
     
     class Config:
         schema_extra = {
             "example": {
-                "title": "Updated title",
-                "content": "updated content"
+                "title": "UpdatedContent",
+                "content": "Test!",
+                "date": "08/24/2023",
+                "happythings": "Enjoyed a walk in the park.",
+                "waterintake": 5,
+                "todaysmood": 4,
+                "selfcareActivities": "Read a book",
+                "Breakfast": "Oatmeal",
+                "Lunch": "Salad",
+                "Dinner": "Grilled chicken",
+                "Snacks": "Fruit",
+                "Anxious": "Managed stress through deep breathing",
+                "Sad": "Something reminded me of my ex",
             }
         }
